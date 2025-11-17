fd5596dd3efba264363d7694b4930d3476a82a69
Red Chien <redchien@viridiasolutions.com>
2025-11-17 16:52:24 +0800

    feat: NotificationService with snackbar; Install ngrx/operator for using tapResponse operator.

---
+++ src/app/services/notifications-service.ts
@@ -0,0 +1,29 @@
+import { inject, Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NotificationsService {
+  private snackbar = inject(MatSnackBar);
+
+  success(message: string) {
+    this.openSnackbar(message, 'OK', 'success-snackbar');
+  }
+
+  error(message: string) {
+    this.openSnackbar(message, 'OK', 'error-snackbar');
+  }
+
+  private openSnackbar(
+    message: string,
+    action: string,
+    className = '',
+    duration = 50000,
+  ) {
+    this.snackbar.open(message, action, {
+      duration,
+      panelClass: [className],
+    });
+  }
+}
